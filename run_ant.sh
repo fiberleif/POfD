a9bee4d6c4c7e63406e3774bcc3349ccf3119241
fiberleif <fiberleif@gmail.com>
2019-06-05 19:20:13 +0800

    Update run files for multiple reward_coeffs.

--- run_ant.sh
+++ run_ant.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+REWARD_COEFFS="0.0 0.2 0.4 0.6 0.8 1.0"
 SEEDS="0 1 2 3 4"
-for SEED in $SEEDS
+for REWARD_COEFF in $REWARD_COEFFS
 do
-	python baselines/gail/run_mujoco.py --env_id Ant-v1 --seed $SEED --num_epochs 400 --reward_coeff 0.0 --timesteps_per_batch 5000 --eval_interval 4 --expert_path dataset/ant.npz
+    for SEED in $SEEDS
+    do
+	    python baselines/gail/run_mujoco.py --env_id Ant-v1 --seed $SEED --num_epochs 400 --reward_coeff 0.0 --timesteps_per_batch 5000 --eval_interval 4 --expert_path dataset/ant.npz
+    done
 done
--- run_walker2d.sh
+++ run_walker2d.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+REWARD_COEFFS="0.0 0.2 0.4 0.6 0.8 1.0"
 SEEDS="0 1 2 3 4"
-for SEED in $SEEDS
+for REWARD_COEFF in $REWARD_COEFFS
 do
-	python baselines/gail/run_mujoco.py --env_id Walker2d-v1 --seed $SEED --num_epochs 1000 --reward_coeff 0.0 --expert_path dataset/walker.npz
+    for SEED in $SEEDS
+    do
+        python baselines/gail/run_mujoco.py --env_id Walker2d-v1 --seed $SEED --num_epochs 1000 --reward_coeff $REWARD_COEFF --expert_path dataset/walker.npz
+    done
 done
